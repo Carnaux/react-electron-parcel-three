a5daa9ebd5265900f72e8ff64457885085046f76
Daniel Fernandes Gonçalves de Oliveira <danielfeenandes@gmail.com>
2018-11-10 15:59:21 -0200

    Upload files

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ThreeScene from "./components/three";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ThreeScene />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
